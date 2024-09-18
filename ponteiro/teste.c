ca41e237010793e1f817de1fb86187e2bf675021
FelipeRangel77 <felipealbuquerquerangel@gmail.com>
2024-09-18 12:55:08 +0000

    update

---
+++ ponteiro/teste.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+//     void troca(int *pa,int *pb);
+//     int main(void){
+//         int a = 10 , b=20
+//         troca(&a,&b);
+//         printf("a = %d b = %d", a,b);
+//         return 0;
+// }
+//     void troca(int *pa, int *pb){
+//         int tmp = *pb
+//         *pb = *pa
+//         *pa = tmp
+// }
+
+//Numero 1
+
+void maiorMenor(vetor[]);
+
+int main(){
+    int vetor[4]
+    maiorMenor(vetor[]);
+    for(int i = 0; i < 4; i++){
+        printf("%d", vetor[i])
+    }
+    return 0;
+}
+
+    void maiorMenor(int vetor[]){
+        for(int i = 0; i < 4; i++){
+            *(vetor + i) = i;
+    }
+}
