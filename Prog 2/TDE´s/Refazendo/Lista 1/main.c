7b2e4e765df3013f78168df929135342aa0bd19c
FelipeRangel77 <felipealbuquerquerangel@gmail.com>
2025-06-24 19:51:22 -0300

    update

---
+++ Prog 2/TDE´s/Refazendo/Lista 1/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+    //! Questao 1
+    // Faça o cálculo a seguir, considerando a matriz A 2x4. Os valores devem ser perguntados ao usuário:
+    // Matriz A.
+
+    int MatrizA[2][4];
+
+    
+    return 0;
+}
